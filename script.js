20f05ad53f14fdec251be0a25b2c9132a4d7fd59
Annas03 <baigannas9@gmail.com>
2022-08-13 15:22:25 +0500

    second-commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let AddBtnStatus = false
 let AddTransaction = document.getElementById("add-transaction")
-let TransactionList = [
-    {desc:"Salary", Amount:10000, type:"income"},
-    {desc:"Cinema", Amount:50, type:"expense"}
-]
+let TransactionList = []
 let TransactionDiv = document.getElementsByClassName('transactions-list')
 
 document.getElementById("add-btn").addEventListener("click", AddBtnClicked)
@@ -35,8 +32,9 @@ function UpdateTransactionList(){
     document.getElementById("expense").checked = false
     document.getElementById("income").checked = false
 
+    document.getElementById("search-bar").value = null
     UpdateBalance()
-    // UpdateTransactionListUI()
+    UpdateTransactionListUI(TransactionList)
 
 }
 
@@ -73,8 +71,23 @@ function UpdateIncome(transaction){
     element.textContent = total.toString()
 }
 
+function UpdateTransactionListUI(Transactions){
+    let element = document.getElementById("transactions-list")
+    let last_entry = Transactions.pop()
+    element.innerHTML += '<div class="transaction border-2 rounded-lg mt-2 px-5 flex justify-between"><h4 class = "py-1 text-lg font-semibold">'+last_entry.desc+'</h4><p class = "py-1 text-lg font-semibold">$'+last_entry.Amount+'</p></div>'
+    Transactions.push(last_entry)
+}
+
 function SearchList(){
+    let SearchBar = document.getElementById("search-bar")
+    let element = document.getElementById("transactions-list")
     for(let i=0; i<TransactionList.length; i++){
-        TransactionList[i].desc.toLowerCase().search(document.getElementById("search-bar").value.toLowerCase()) != -1 ? console.log(TransactionList[i].desc) : console.log(i)
+        if(TransactionList[i].desc.toLowerCase().search(SearchBar.value.toLowerCase()) != -1){
+            element.innerHTML = '<div class="transaction border-2 rounded-lg mt-2 px-5 flex justify-between"><h4 class = "py-1 text-lg font-semibold">'+TransactionList[i].desc+'</h4><p class = "py-1 text-lg font-semibold">$'+TransactionList[i].Amount+'</p></div>'
+        }
+        else if(SearchBar.value == null){
+            UpdateTransactionListUI(TransactionList)
+            break
+        }
     }
 }
