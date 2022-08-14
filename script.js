e42ca553f6e9c04839825b75522941001af285e5
Annas03 <baigannas9@gmail.com>
2022-08-14 13:08:40 +0500

    second-commit

--- script.js
+++ script.js
@@ -79,15 +79,21 @@ function UpdateTransactionListUI(Transactions){
 }
 
 function SearchList(){
+    console.log("funciton called")
     let SearchBar = document.getElementById("search-bar")
     let element = document.getElementById("transactions-list")
-    for(let i=0; i<TransactionList.length; i++){
-        if(TransactionList[i].desc.toLowerCase().search(SearchBar.value.toLowerCase()) != -1){
-            element.innerHTML = '<div class="transaction border-2 rounded-lg mt-2 px-5 flex justify-between"><h4 class = "py-1 text-lg font-semibold">'+TransactionList[i].desc+'</h4><p class = "py-1 text-lg font-semibold">$'+TransactionList[i].Amount+'</p></div>'
+    if(SearchBar.value == ""){
+        element.innerHTML = null
+        for(let i=0; i<TransactionList.length; i++){
+            element.innerHTML += '<div class="transaction border-2 rounded-lg mt-2 px-5 flex justify-between"><h4 class = "py-1 text-lg font-semibold">'+TransactionList[i].desc+'</h4><p class = "py-1 text-lg font-semibold">$'+TransactionList[i].Amount+'</p></div>'
         }
-        else if(SearchBar.value == null){
-            UpdateTransactionListUI(TransactionList)
-            break
+    }
+    else{
+        element.innerHTML = null
+        for(let i=0; i<TransactionList.length; i++){
+            if(TransactionList[i].desc.toLowerCase().search(SearchBar.value.toLowerCase()) != -1){
+                element.innerHTML += '<div class="transaction border-2 rounded-lg mt-2 px-5 flex justify-between"><h4 class = "py-1 text-lg font-semibold">'+TransactionList[i].desc+'</h4><p class = "py-1 text-lg font-semibold">$'+TransactionList[i].Amount+'</p></div>'
+            }
         }
     }
 }
